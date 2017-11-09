740ba8a05637cc559be2897dc4eeb36a5c52157b
mandov <gabi_nm@abv.bg>
2017-11-09 16:55:09 +0200

    Pushing Variables-Data-Expressions-and-Constants Homework

---
+++ 04.Variables-Data-Expressions-and-Constants/01.ClassSizeInCSharp/Size.cs
@@ -0,0 +1,30 @@
+﻿namespace VariablesDataExpressionsAndConstantsHomeworkTaskOne
+{
+    using System;
+
+    public class Size
+    {
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width { get; private set; }
+
+        public double Height { get; private set; }
+
+        public static Size GetRotatedSize(Size objectToRotate, double rotationAngle)
+        {
+            double rotationAngleCos = Math.Cos(rotationAngle);
+            double rotationAngleSin = Math.Sin(rotationAngle);
+            double rotationCosAbs = Math.Abs(rotationAngleCos);
+            double rotationSinAbs = Math.Abs(rotationAngleSin);
+
+            double width = (rotationCosAbs * objectToRotate.Width) + (rotationSinAbs * objectToRotate.Height);
+            double height = (rotationSinAbs * objectToRotate.Width) + (rotationCosAbs * objectToRotate.Height);
+
+            return new Size(width, height);
+        }
+    }
+}
---
+++ 04.Variables-Data-Expressions-and-Constants/02.MethodPrintStatisticsInCSharp/PrintStatistics.cs
@@ -0,0 +1,59 @@
+﻿namespace VariablesDataExpressionsAndConstantsHomeworkTaskTwo
+{
+    using System;
+
+    public class Statistics
+    {
+        public double[] ProcessStatistics(double[] collection)
+        {
+            if (collection.Length == 0)
+            {
+                throw new ArgumentException("Cannot process statistics without any input data!");
+            }
+
+            double minValue = collection[0];
+            double maxValue = collection[0];
+            double collectionSum = 0;
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (collection[i] < minValue)
+                {
+                    minValue = collection[i];
+                }
+
+                if (collection[i] > maxValue)
+                {
+                    maxValue = collection[i];
+                }
+
+                collectionSum += collection[i];
+            }
+
+            double average = collectionSum / collection.Length;
+
+            return new double[] { minValue, maxValue, average };
+        }
+
+        public void PrintStatistics(double[] processedStatistics)
+        {
+            if (processedStatistics.Length != 3)
+            {
+                throw new ArgumentException("The processed statistics has 3 elements - min, max and average");
+            }
+
+            double minValue = processedStatistics[0];
+            double maxValue = processedStatistics[1];
+            double average = processedStatistics[2];
+
+            this.Printer(minValue);
+            this.Printer(maxValue);
+            this.Printer(average);
+        }
+
+        private void Printer(double number)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
