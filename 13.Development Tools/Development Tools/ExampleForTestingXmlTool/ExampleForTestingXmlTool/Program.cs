626fa2fdf8adc2669314c929842e26618d1642e2
mandov <gabi_nm@abv.bg>
2017-11-15 23:40:46 +0200

    Pushing Development-Tools Homework

---
+++ 13.Development Tools/Development Tools/ExampleForTestingXmlTool/ExampleForTestingXmlTool/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+/// <summary>
+/// Example
+/// </summary>
+namespace ExampleForTestingXmlTool
+{
+    public class Calculations
+    {
+        static void Main()
+        {
+        }
+        /// <summary>
+        /// Sum a parameter with b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Sum(double a, double b)
+        {
+            double result = a + b;
+            return result;
+        }
+
+        /// <summary>
+        /// Subtract a parametar with b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Subtract(double a, double b)
+        {
+            double result = a - b;
+            return result;
+        }
+    }
+}
---
+++ 13.Development Tools/Development Tools/ExampleT4Template/TestT4Template/T4TemplateExample.cs
@@ -0,0 +1,31 @@
+﻿
+public class T4Demo
+{
+	
+		public string Name1 { get; set; }
+
+	
+		public string Name2 { get; set; }
+
+	
+		public string Name3 { get; set; }
+
+	
+		public string Name4 { get; set; }
+
+	
+		public string Name5 { get; set; }
+
+	
+		public string Name6 { get; set; }
+
+	
+		public string Name7 { get; set; }
+
+	
+		public string Name8 { get; set; }
+
+	
+		public string Name9 { get; set; }
+
+	}
